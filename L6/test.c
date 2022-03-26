49232dd1148dbd623a318580e66c0366dd033f50
Mazaud Thomas <thomas.mazaud@epitech.eu>
2022-03-26 23:45:31 +0100

    Added missing test

---
+++ L6/test.c
@@ -0,0 +1,15 @@
+/*
+** EPITECH PROJECT, 2021
+** B-CPE-110-LYN-1-1-pushswap-thomas.mazaud
+** File description:
+** PushSwap
+*/
+
+#include "../includes/pushswap.h"
+
+int test(void)
+{
+    printf("Testing");
+
+    printf("Testing");
+}
---
+++ V1/1/test.h
@@ -0,0 +1,22 @@
+/*
+** EPITECH PROJECT, 2021
+** B-CPE-110-LYN-1-1-pushswap-thomas.mazaud
+** File description:
+** pushswap
+*/
+
+#ifndef TEST_H_
+    #define TEST_H_
+
+    #include <stdio.h>
+    #include <stdlib.h>
+    #include <unistd.h>
+
+    //Structures double circular linked list
+    typedef struct list {
+        int data;
+        struct list *next;
+        struct list *prev;
+    } list;
+
+#endif /* !TEST_H_ */
