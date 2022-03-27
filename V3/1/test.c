8451e7da24b5ce43443c3d2a33fc7c3cc3520b97
Mazaud Thomas <thomas.mazaud@epitech.eu>
2022-03-27 16:30:42 +0200

    [ADD] V3

---
+++ V3/1/test.c
@@ -0,0 +1,13 @@
+/*
+** EPITECH PROJECT, 2021
+** B-CPE-110-LYN-1-1-pushswap-thomas.mazaud
+** File description:
+** PushSwap
+*/
+
+#include "../includes/pushswap.h"
+
+int function_1(void)
+{
+    int* a;
+}
