63c16161e17d525f8624f2031f549f750617545f
Baptiste Leroyer <leroyerbaptiste@gmail.com>
2022-10-19 12:06:42 +0200

    :white_check_mark: add ok test H2

--- H2/1/OK/test.h
+++ H2/1/OK/test.h
@@ -1,17 +1,22 @@
 /*
-** EPITECH PROJECT, 2021
-** B-CPE-110-LYN-1-1-pushswap-thomas.mazaud
+** EPITECH PROJECT, 2022
+** Abricot-Tests
 ** File description:
-** pushswap
+** test
 */
+
+#ifndef TEST_H_
+    #define TEST_H_
 
     #include <stdio.h>
     #include <stdlib.h>
     #include <unistd.h>
 
-    //Structures double circular linked list
-    typedef struct list {
-        int data;
-        struct list *next;
-        struct list *prev;
-    } list_t;
+//Structures double circular linked list
+typedef struct list {
+    int data;
+    struct list *next;
+    struct list *prev;
+} list_t;
+
+#endif /* !TEST_H_ */
